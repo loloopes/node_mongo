fa59d3e653dde8060eb6e43d112fe25db170371a
Lopes <gus.lcampos38@gmail.com>
2021-12-06 21:53:29 -0300

    prob was spelling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const error = require('./middleware/error');
 const root = require('./controller/router');
 
 const app = express();
-const PORT = proces.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
